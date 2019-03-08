cb24106412b4a882bc725e882dd68586693cf4a6
Shayne Mujuru <shaynemujuru@gmail.com>
2019-03-08 10:20:42 -0700

    testing remote

---
+++ Inspector.java
@@ -0,0 +1,18 @@
+/**
+    NAME : SHAYNE MUJURU
+    UCID : 30029552
+    TITLE: CPSC501 AII : INSPECTOR CLASS 
+    DESCRIPTION : Goal is to create a reflective object inspector 
+                    that does a complete introspection of 
+                    an object at runtime.
+
+**/ 
+
+import java.lang.reflect.*;
+
+public class Inspector {
+
+    public void inspect(Object obj, boolean recursive) {
+        
+    }
+}
